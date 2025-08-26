fbf9655984983303a7b22ac684f5408fed390073
SalimIwiqja <salimemaataoui@gmail.com>
2025-08-26 06:57:53 +0100

    Update ytdl-fast.js

--- plugins/ytdl-fast.js
+++ plugins/ytdl-fast.js
@@ -63,7 +63,7 @@ malvin({
                                 document: { url: outputPath },
                                 mimetype: 'audio/mp3',
                                 fileName: `${title}.mp3`,
-                                caption: `🎶 *Title:* ${vid.title}\n⏱️ *Duration:* ${duration}\n\n> Powered By Lucky Tech Hub`,
+                                caption: `🎶 *Title:* ${vid.title}\n⏱️ *Duration:* ${duration}\n\n> Powered By Salim`,
                                 thumbnail: { url: thumbnail }
                             }, { quoted: mek });
                             fs.unlinkSync(outputPath);
