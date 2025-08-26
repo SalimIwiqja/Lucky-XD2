a225ea58caad4b63bea622c626a4a4659e038db2
SalimIwiqja <salimemaataoui@gmail.com>
2025-08-26 06:55:10 +0100

    Update ytdlvid.js

--- plugins/ytdlvid.js
+++ plugins/ytdlvid.js
@@ -51,7 +51,7 @@ malvin({
         await conn.sendMessage(from, {
             video: { url: downloadUrl },
             mimetype: "video/mp4",
-            caption: `🎬 *Video Downloaded Successfully!*\n\n> Powered By Lucky Tech Hub`
+            caption: `🎬 *Video Downloaded Successfully!*\n\n> Powered By Salim`
         }, { quoted: mek });
 
     } catch (error) {
